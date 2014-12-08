fb0c0e34bdd094e65e85677d351acd4ffb887b14
prashant <prashant1988mishra@gmail.com>
2014-12-08 15:45:44 +0530

    update with user image

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,10 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :conversations, :foreign_key => :sender_id       
+
+  def gravatar_url
+    downcased_stripped_email=email.strip.downcase
+    hash= Digest::MD5.hexdigest(downcased_stripped_email)
+    "http://gravatar.com/avatar/#{hash}" 
+  end
 end
